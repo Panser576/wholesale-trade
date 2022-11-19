3a5158c32b5760b7504ec8c8d97bcec959cb2732
Panser576 <d.lyutenko@yandex.ru>
2022-11-19 15:00:12 +0200

    test1

---
+++ src/main/java/org/example/wholesale_trade/ServingWebContentApplication.java
@@ -0,0 +1,11 @@
+package org.example.wholesale_trade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServingWebContentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServingWebContentApplication.class, args);
+    }
+}
