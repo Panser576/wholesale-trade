6d61cc1b9095ab699db3055f3d8f51c9a6b52cdf
Nort <vk-pt@yandex.ru>
2022-11-29 19:04:36 +0200

    security
    пока так

--- src/main/java/org/example/wholesale_trade/controller/BlogController.java
+++ src/main/java/org/example/wholesale_trade/controller/BlogController.java
@@ -1,6 +1,5 @@
 package org.example.wholesale_trade.controller;
 
-import org.example.wholesale_trade.repository.postRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class BlogController {
 
     @Autowired
-    private postRepository postRepository;
+    private org.example.wholesale_trade.repository.postRepository postRepository;
     @GetMapping("/prais")
     public String praisMain(Model mobel) {
         return "prais-main";
---
+++ src/main/java/org/example/wholesale_trade/domein/Role.java
@@ -0,0 +1,8 @@
+package org.example.wholesale_trade.domein;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role  {
+    USER, ADMIN;
+
+}
