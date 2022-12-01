071e736b353b13f2ef74d50ce9612953a4cc48f3
Panser576 <d.lyutenko@yandex.ru>
2022-12-01 15:31:44 +0200

    add perehod id

--- src/main/java/org/example/wholesale_trade/controller/BlogController.java
+++ src/main/java/org/example/wholesale_trade/controller/BlogController.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class BlogController {
@@ -22,7 +26,7 @@ public class BlogController {
     }
     @GetMapping("/prais/add")
     public String praisAdd(Model mobel) {
-        return "prais-add.html";
+        return "prais-add";
     }
 
     @PostMapping("/prais/add")
@@ -31,4 +35,14 @@ public class BlogController {
         postRepository.save(post);
         return "redirect:/prais";
     }
+
+    @GetMapping("/prais/{id}")
+    public String praisInfo(@PathVariable(value = "id") long id, Model mobel) {
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        mobel.addAttribute("post", post);
+        return "prais-info";
+    }
+
 }
